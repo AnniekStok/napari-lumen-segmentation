a990bdab5f3eca1f9aae253b05dbf253208376eb
AnniekStok <anniek.stokkermans@gmail.com>
2024-04-23 14:48:21 +0200

    fix init

--- src/napari_lumen_segmentation/__init__.py
+++ src/napari_lumen_segmentation/__init__.py
@@ -1,6 +1,22 @@
 __version__ = "0.0.1"
 from ._widget import AnnotateLabelsND
+from ._custom_table_widget import ColoredTableWidget, TableWidget
+from ._histogram_widget import HistWidget
+from ._layer_dropdown import LayerDropdown
+from ._skeleton_widget import SkeletonWidget
 
 __all__ = (
     "AnnotateLabelsND",
+    "SkeletonWidget",
+    "ColoredTableWidget",
+    "TableWidget",
+    "HistWidget",
+    "LayerDropdown"
+
+
+
+
+
+
+
 )
